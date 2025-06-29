7948e82591359b5edb09552a05d8dcf743bf3422
Azizur Rahaman <91916057+AzizurRahamanGithub@users.noreply.github.com>
2025-06-30 05:22:03 +0600

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -51,7 +51,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aboutazizur@gmail.com'
-EMAIL_HOST_PASSWORD = 'rctixilnneqgajej'
+EMAIL_HOST_PASSWORD = 'Actixilnneqgajea'
 EMAIL_USE_TLS = True
 
 
